b492f96cf81600fe83f6618ae15bb408ce554dcf
Guilherme <guii_arantes@hotmail.com>
2025-10-12 15:04:51 -0300

    Refactor import statement in InputDate component for consistency

--- src/components/InputDate.jsx
+++ src/components/InputDate.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import '../styles/InputDate.css';
+import "../styles/InputDate.css";
 
 const InputDate = ({ selectedDate, onDateChange, placeholderText, locale = "pt-BR", disabled = false }) => {
   return (
